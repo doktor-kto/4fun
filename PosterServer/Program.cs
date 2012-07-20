ce7552d7569c8c1f43473c12c941e024febd1692
smi13 <fedka@ebanat.com>
2012-07-20 19:24:28 +0400

    first attempt to login

--- PosterServer/Program.cs
+++ PosterServer/Program.cs
@@ -12,11 +12,19 @@ namespace PosterServer
     {
         public static void Main()
         {
+            SeleniumPoster ps = new SeleniumPoster();
+
+            ps.login("slando.ru", "iamdiligentstudent@narod.ru", "86420555");
+            ps.login("olx.ru", "iamdiligentstudent@narod.ru", "86420555");
+
             Server s = new Server();
             Parser par = new Parser();
 
-            s.start("192.168.0.101", 5050);
+            if (!s.start("192.168.0.104", 5050))
+                return;
+
             s.acceptConnection();
+            
 
 
 
--- PosterServer/SeleniumPoster.cs
+++ PosterServer/SeleniumPoster.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+// Requires reference to WebDriver.Support.dll
+using OpenQA.Selenium.Support.UI;
+
 namespace PosterServer
 {
     class SeleniumPoster
     {
+        private IWebDriver driver;
+
         public SeleniumPoster()
         {
-
+            String path = "d:\\Programs\\Mozilla Firefox\\firefox.exe";
+            driver = new FirefoxDriver(new FirefoxBinary(path), null);
         }
 
         public void postAdvert( String site, Advert adv )
@@ -17,15 +26,50 @@ namespace PosterServer
 
         }
 
-        public void login( String site )
+        public void login( String site, String mail, String password )
         {
             if (String.Compare("slando.ru", site) == 0)
             {
+                String slando_path = 
+                    "http://slando.ru/account/?ref[0][action]=user&ref[0][method]=index";
+                driver.Navigate().GoToUrl(slando_path);
+
+                IWebElement mail_elem = driver.FindElement(By.Id("userEmail"));
+
+                mail_elem.Click();
+                mail_elem.SendKeys(mail);
+
+                IWebElement pass_elem = driver.FindElement(By.Id("userPass"));
+
+                pass_elem.Click();
+                pass_elem.SendKeys(password);
+
+                IWebElement login_form = driver.FindElement(By.Id("se_userLogin"));
+
+                login_form.Click();
+                
                 return;
             }
 
             if (String.Compare("olx.ru", site) == 0)
             {
+                String slando_path = "http://www.olx.ru/login.php";
+                driver.Navigate().GoToUrl(slando_path);
+
+                IWebElement mail_elem = driver.FindElement(By.Id("username"));
+
+                mail_elem.Click();
+                mail_elem.SendKeys(mail);
+
+                IWebElement pass_elem = driver.FindElement(By.Id("password"));
+
+                pass_elem.Click();
+                pass_elem.SendKeys(password);
+
+                IWebElement login_form = driver.FindElement(By.Id("submit"));
+
+                login_form.Click();
+                         
                 return;
             }
             
@@ -42,7 +86,7 @@ namespace PosterServer
 
         ~SeleniumPoster()
         {
-
+            driver.Quit();
         }
     }
 }
