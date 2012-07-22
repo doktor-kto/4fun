c0df7e053f8c0b74e7638b53cd4f2e77bbc961ca
smi13 <fedka@ebanat.com>
2012-07-22 05:18:15 +0400

    ad posting on olx.ru complete (no images)

--- PosterServer/Program.cs
+++ PosterServer/Program.cs
@@ -14,10 +14,34 @@ namespace PosterServer
         {
             SeleniumPoster ps = new SeleniumPoster();
 
-            ps.login("avito.ru", "iamdiligentstudent@narod.ru", "86420555");
+            /*ps.login("avito.ru", "iamdiligentstudent@narod.ru", "86420555");
             ps.login("restate.ru", "iamdiligentstudent@narod.ru", "86420555");
-            ps.login("slando.ru", "iamdiligentstudent@narod.ru", "86420555");
+            ps.login("slando.ru", "iamdiligentstudent@narod.ru", "86420555");*/
             ps.login("olx.ru", "iamdiligentstudent@narod.ru", "86420555");
+
+            /*class Advert
+            {
+                public int roomNumber;
+                public int price;
+                public string name;
+                public string desc;
+                public string district;
+                public string subway_station;
+                public string street;
+                public string house;
+                public string square;
+                public int floor;
+                public int floorNumber;
+                public string phone;        */
+
+            ps.postAdvert(new Advert(3, 20000, "Квартира в хорошем состоянии", 
+                "Отличная квартира", 
+                "Адмиралтейский",
+                "Сенная площадь",
+                "Садовая",
+                "24",
+                "50",
+                 3, 5, "891239000", "iamdiligentstudent@narod.ru"));
 
             Server s = new Server();
             Parser par = new Parser();
--- PosterServer/SeleniumPoster.cs
+++ PosterServer/SeleniumPoster.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-
 // Requires reference to WebDriver.Support.dll
 using OpenQA.Selenium.Support.UI;
+using Selenium;
+
+// Activate an application window.
+
 
 namespace PosterServer
 {
+    static class Extension
+    {
+        public static IJavaScriptExecutor Scripts(this IWebDriver driver)
+        {
+            return (IJavaScriptExecutor)driver;
+        }
+    }
+
     class SeleniumPoster
     {
         private String firefox_path = "d:\\Programs\\Mozilla Firefox\\firefox.exe";
@@ -19,9 +32,11 @@ namespace PosterServer
         private IWebDriver olx_driver;
         private IWebDriver restate_driver;
 
+      
+
         public SeleniumPoster()
         {
-            slando_driver = new FirefoxDriver(new FirefoxBinary(firefox_path), null);
+            /*slando_driver = new FirefoxDriver(new FirefoxBinary(firefox_path), null);
             avito_driver = new FirefoxDriver(new FirefoxBinary(firefox_path), null);
             olx_driver = new FirefoxDriver(new FirefoxBinary(firefox_path), null);
             restate_driver = new FirefoxDriver(new FirefoxBinary(firefox_path), null);
@@ -30,18 +45,86 @@ namespace PosterServer
                     "http://slando.ru/account/?ref[0][action]=user&ref[0][method]=index";
             slando_driver.Navigate().GoToUrl(slando_path);
 
-            String olx_path = "http://www.olx.ru/login.php";
-            olx_driver.Navigate().GoToUrl(olx_path);
+            
 
             String avito_path = "http://www.avito.ru/profile";
             avito_driver.Navigate().GoToUrl(avito_path);
 
             String restate_path = "http://www.restate.ru";
-            restate_driver.Navigate().GoToUrl(restate_path);
+            restate_driver.Navigate().GoToUrl(restate_path);*/
+
+            olx_driver = new FirefoxDriver(new FirefoxBinary(firefox_path), null);
+
+            String olx_path = "http://www.olx.ru/login.php";
+            olx_driver.Navigate().GoToUrl(olx_path);
         }
 
-        public void postAdvert( String site, Advert adv )
+        private void _postSlando( Advert adv )
         {
+
+        }
+
+        private void _postAvito( Advert adv )
+        {
+
+        }
+
+        public static void Delay(int Seconds)
+            {
+                System.Threading.Thread.Sleep(Seconds * 1000);
+           }
+
+        private void _postOlx( Advert adv )
+        {
+            olx_driver.Navigate().GoToUrl("http://www.olx.ru/posting.php");
+            olx_driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+
+            olx_driver.FindElement(By.Id("upload-wrapper")).Click();
+            
+            /*
+            olx_driver.FindElement(By.Id("regionSelectorChangeMode")).Click();
+            Delay(2);
+            new SelectElement(olx_driver.FindElement(By.Id("state"))).SelectByText("Санкт-Петербург");
+            new SelectElement(olx_driver.FindElement(By.Id("city"))).SelectByText("Санкт-Петербург");
+            new SelectElement(olx_driver.FindElement(By.Id("categoryParent"))).SelectByText("Недвижимость");
+            new SelectElement(olx_driver.FindElement(By.Id("categoryChild"))).SelectByText("Аренда квартир, домов");
+            olx_driver.FindElement(By.Id("title")).Clear();
+            olx_driver.FindElement(By.Id("title")).SendKeys(adv.name);
+            olx_driver.FindElement(By.Id("C")).Clear();
+            olx_driver.FindElement(By.Id("C")).SendKeys(adv.price.ToString());
+            olx_driver.FindElement(By.Id("email")).Clear();
+            olx_driver.FindElement(By.Id("email")).SendKeys(adv.e_mail);
+            olx_driver.FindElement(By.Id("phone")).Clear();
+            olx_driver.FindElement(By.Id("phone")).SendKeys(adv.phone);
+            olx_driver.FindElement(By.Id("surface")).Clear();
+            olx_driver.FindElement(By.Id("surface")).SendKeys(adv.square);
+            olx_driver.FindElement(By.Id("streetaddress")).Clear();
+            olx_driver.FindElement(By.Id("streetaddress")).SendKeys(adv.street + ", " + adv.house);
+            new SelectElement(olx_driver.FindElement(By.Id("bedrooms"))).SelectByText(adv.roomNumber.ToString());
+            new SelectElement(olx_driver.FindElement(By.Id("neighborhood"))).SelectByText(adv.subway_station.ToString());
+            olx_driver.FindElement(By.Id("description_ifr")).SendKeys(adv.desc);
+
+            ISelenium selenium = new WebDriverBackedSelenium(olx_driver, "http://www.google.com");
+
+            selenium.Click("//a[@id='selectFiles']");
+            selenium.Type("xpath=//input[@type='file' and @name='userfile']", @"c:\testdata\image.png");
+            //olx_driver.FindElement(By.Id("btnPublish")).Click();*/
+
+
+        }
+
+        private void _postRestate( Advert adv )
+        {
+
+
+        }
+
+        public void postAdvert( Advert adv )
+        {
+            _postOlx(adv);
+            _postRestate(adv);
+            _postAvito(adv);
+            _postSlando(adv);
 
         }
 
